73fb0af08bed1b8c6dbc4233daf064b4bc795ced
Johann Jungbauer <jungbauer@gmail.com>
2021-02-22 16:11:19 +0200

    Initial commit

---
+++ src/main/java/com/jungbauer/consistencycalendar/ConsistencyCalendarApplication.java
@@ -0,0 +1,13 @@
+package com.jungbauer.consistencycalendar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsistencyCalendarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConsistencyCalendarApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jungbauer/consistencycalendar/ConsistencyCalendarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jungbauer.consistencycalendar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsistencyCalendarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
