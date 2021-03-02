82225f88f1dab380590f3a6e1327bd666296aa6a
Johann Jungbauer <jungbauer@gmail.com>
2021-03-02 13:14:23 +0200

    Display for D3 testing I did previously.

--- src/main/java/com/jungbauer/consistencycalendar/HomeController.java
+++ src/main/java/com/jungbauer/consistencycalendar/HomeController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 @Controller
@@ -14,5 +15,14 @@ public class HomeController {
         Map<String,String> map = new HashMap<>();
         map.put("name", "FRED");
         return new ModelAndView("index", map);
+    }
+
+    @GetMapping("/display")
+    public ModelAndView display() {
+        Map<String, Object> model = new LinkedHashMap<>();
+        String title = "DISPLAY";
+        model.put("title", title);
+
+        return new ModelAndView("display", model);
     }
 }
