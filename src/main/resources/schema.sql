cd8753e5871b2be77090ffb41153e6fb01bf15a3
Johann Jungbauer <jungbauer@gmail.com>
2021-03-12 15:14:02 +0200

    Sql file cleanup

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,19 +1,4 @@
-DROP TABLE IF EXISTS billionaires;
-
-CREATE TABLE billionaires
-(
-    id         INT AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(250) NOT NULL,
-    last_name  VARCHAR(250) NOT NULL,
-    career     VARCHAR(250) DEFAULT NULL
-);
-
-CREATE TABLE teststuff
-(
-    id         INT AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(250) NOT NULL
-);
-
+-- USERS is a test and tutorial table.
 CREATE TABLE users
 (
     id      INT AUTO_INCREMENT PRIMARY KEY,
