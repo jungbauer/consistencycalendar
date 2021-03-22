79e611ec27fd83ea3c30fa44b0d05795ec405177
Johann Jungbauer <jungbauer@gmail.com>
2021-03-22 15:02:01 +0200

    test: add obj creation test for DisplayService

--- src/test/groovy/com/jungbauer/consistencycalendar/DisplayServiceSpec.groovy
+++ src/test/groovy/com/jungbauer/consistencycalendar/DisplayServiceSpec.groovy
@@ -1,5 +1,8 @@
 package com.jungbauer.consistencycalendar
 
+import com.jungbauer.consistencycalendar.database.Completion
+import com.jungbauer.consistencycalendar.database.Habit
+import com.jungbauer.consistencycalendar.object.HabitDisplay
 import com.jungbauer.consistencycalendar.object.Month
 import com.jungbauer.consistencycalendar.service.DisplayService
 import spock.lang.Shared
@@ -60,5 +63,42 @@ class DisplayServiceSpec extends Specification {
         months.get(0).completions.size() == 10
         months.get(1).completions.size() == 1
         months.get(2).completions.size() == 1
+    }
+
+    def "createHabitDisplay()"() {
+        Habit habit = new Habit()
+        habit.setId(15)
+        habit.setType("testType")
+        habit.setTitle("Testing Stuff")
+        habit.setNote("Notes go here")
+        habit.setStartDate(LocalDate.of(2021,2,1))
+        habit.setEndDate(LocalDate.of(2021,3,31))
+
+        List<String> stringDates = List.of("2021-02-04","2021-02-05",
+                "2021-02-07","2021-02-22","2021-03-01")
+        List<Completion> completions = new ArrayList<>()
+
+        for (String date : stringDates) {
+            Completion completion = new Completion()
+            completion.setDate(LocalDate.parse(date))
+            completions.add(completion)
+        }
+
+        habit.setCompletions(completions)
+
+        HabitDisplay hb = displayService.createHabitDisplay(habit)
+
+        expect:
+        hb.id == 15
+        hb.type == "testType"
+        hb.title == "Testing Stuff"
+        hb.note == "Notes go here"
+        hb.startDate.isEqual(LocalDate.of(2021,2,1))
+        hb.endDate.isEqual(LocalDate.of(2021,3,31))
+        hb.months.size() == 2
+        hb.months[0].firstDay.isEqual(LocalDate.of(2021,2,1))
+        hb.months[0].completions.size() == 4
+        hb.months[1].firstDay.isEqual(LocalDate.of(2021,3,1))
+        hb.months[1].completions.size() == 1
     }
 }
